75ed40bb6df46db0996164c96e39ca63943df6b1
zarmet <borntoski811@gmail.com>
2021-11-29 17:26:17 -0500

    Challenges

---
+++ Challenges/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,12 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+
